fcf3822f9c57371b8fe237c9e88469f015a1097b
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-07-20 13:18:43 +0100

    Avatar for people

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import personPlaceholderImage from '../assets/personPlaceholderImage.png'
+
+const Avatar = styled.img`
+  width:50px;
+  height: 50px;
+  border-radius: 25px;
+  display: block;
+`;
 
 class Person extends Component {
   render() {
     return (
       <div>
         <b>People</b>
+        <Avatar src={personPlaceholderImage} />
         <div>{this.props.firstName} {this.props.surname}</div>
         <div>{this.props.role}</div>
       </div>
