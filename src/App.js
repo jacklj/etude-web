d59cfb454f95add31e42790bab0a274fe0736b42
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-07-20 12:08:34 +0100

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
+import styled from 'styled-components';
+
 import eventData from './eventData';
 import Event from './components/Event';
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: palevioletred;
+`;
+
+const Header = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Singprocess</h1>
-        </header>
+        <Header>
+          <Title>Stanza</Title>
+        </Header>
         <div>
           {eventData.map(event => <Event
             name={event.name}
