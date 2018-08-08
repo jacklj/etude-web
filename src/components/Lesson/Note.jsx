561cf97620522f8fc276d7b571568f616aeab805
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-08-08 17:12:08 +0100

    Display and edit note component states

--- src/components/Lesson/Note.jsx
+++ src/components/Lesson/Note.jsx
@@ -4,13 +4,43 @@ import PropTypes from 'prop-types';
 import { Card } from '../common/itemCards';
 
 class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+    };
+
+    this.editNote = this.editNote.bind(this);
+    this.saveNote = this.saveNote.bind(this);
+  }
+
+  editNote() {
+    this.setState({ editing: true });
+  }
+
+  saveNote() {
+    this.setState({ editing: false });
+  }
+
   render() {
     const { note, score, type } = this.props;
+    const { editing } = this.state;
     return (
       <Card>
-        <div>{note}</div>
-        <div>{score}</div>
-        <div>{type}</div>
+        {editing ? (
+          <div>
+            <div>editing</div>
+            <button type="submit" onClick={this.saveNote}>Save</button>
+          </div>
+        ) : (
+          <div>
+            <div>{note}</div>
+            <div>{score}</div>
+            <div>{type}</div>
+            <button type="button" onClick={this.editNote}>Edit</button>
+          </div>
+        )
+        }
       </Card>
     );
   }
