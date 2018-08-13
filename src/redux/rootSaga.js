6291e6829e9211b74c30c97737290d18bf04fe92
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-08-13 23:31:23 +0100

    Rename allEventsSaga

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
 
-import fetchAllEventsSaga from './events/events.saga';
+import fetchAllEventsSaga from './events/fetchAllEvents.saga';
 import fetchLocationsSaga from './locations/locations.saga';
 import fetchPeopleSaga from './people/people.saga';
 
