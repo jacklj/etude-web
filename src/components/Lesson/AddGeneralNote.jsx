f61a713be93383f6bf1f02bedc2ffcb37e4b430e
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-08-16 16:56:51 +0100

    Add note nicer UX

--- src/components/Lesson/AddGeneralNote.jsx
+++ src/components/Lesson/AddGeneralNote.jsx
@@ -10,10 +10,12 @@ class AddGeneralNote extends Component {
     this.state = {
       note: '',
       score: '',
+      editingNewNote: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.startEditing = this.startEditing.bind(this);
   }
 
   handleSubmit(event) {
@@ -28,6 +30,9 @@ class AddGeneralNote extends Component {
       event_id: eventId,
     };
     this.props.generalNoteCreateRequest(newNote, eventId);
+    this.setState({
+      editingNewNote: false,
+    });
   }
 
   handleChange(event) {
@@ -39,15 +44,30 @@ class AddGeneralNote extends Component {
     });
   }
 
+  startEditing() {
+    this.setState({
+      note: '',
+      score: '',
+      editingNewNote: true,
+    });
+  }
+
   render() {
-    const { note, score } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <textarea name="note" value={note} onChange={this.handleChange} />
-        <textarea name="score" value={score} onChange={this.handleChange} />
-        <input type="submit" value="Add note" />
-      </form>
-    );
+    const { note, score, editingNewNote } = this.state;
+    const { isCreatingNote } = this.props;
+    let jsx;
+    if (editingNewNote || isCreatingNote) {
+      jsx = (
+        <form onSubmit={this.handleSubmit}>
+          <textarea name="note" value={note} onChange={this.handleChange} />
+          <textarea name="score" value={score} onChange={this.handleChange} />
+          {isCreatingNote ? <span>Saving...</span> : <input type="submit" value="Add note" />}
+        </form>
+      );
+    } else {
+      jsx = <button type="button" onClick={this.startEditing}>Add new note</button>;
+    }
+    return jsx;
   }
 }
 
@@ -56,11 +76,14 @@ AddGeneralNote.defaultProps = {
 };
 
 AddGeneralNote.propTypes = {
+  isCreatingNote: PropTypes.bool.isRequired,
   eventId: PropTypes.number,
   generalNoteCreateRequest: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = () => ({});
+const mapStateToProps = state => ({
+  isCreatingNote: state.notes.creatingNote,
+});
 
 const mapDispatchToProps = {
   generalNoteCreateRequest,
