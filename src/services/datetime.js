431b844c3261d0c50b3e4f4de3aca30212049f5d
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-08-18 16:08:53 +0100

    Display 'in progress' for events in Timeline

--- src/services/datetime.js
+++ src/services/datetime.js
@@ -2,23 +2,27 @@ import moment from 'moment';
 
 export const renderDuration = (startDateTime, endDateTime) => {
   const start = moment(startDateTime);
-  const end = moment(endDateTime);
-
   let startEndRender;
-  if (start.isSame(end, 'day')) {
-    startEndRender = `${start.format('H:mm')} - ${end.format('H:mm')} ${start.format(
-      'dddd Do MMMM YYYY',
-    )}`;
-  } else if (start.isSame(end, 'month')) {
-    startEndRender = `${start.format('H:mm dddd Do')} - ${end.format('H:mm dddd Do MMMM YYYY')}`;
-  } else if (start.isSame(end, 'year')) {
-    startEndRender = `${start.format('H:mm dddd Do MMMM')} - ${end.format(
-      'H:mm dddd Do MMMM YYYY',
-    )}`;
+
+  if (!endDateTime) {
+    startEndRender = `${start.format('H:mm dddd Do MMMM YYYY')} - in progress`;
   } else {
-    startEndRender = `${start.format('H:mm dddd Do MMMM YYYY')} - ${end.format(
-      'H:mm dddd Do MMMM YYYY',
-    )}`;
+    const end = moment(endDateTime);
+    if (start.isSame(end, 'day')) {
+      startEndRender = `${start.format('H:mm')} - ${end.format('H:mm')} ${start.format(
+        'dddd Do MMMM YYYY',
+      )}`;
+    } else if (start.isSame(end, 'month')) {
+      startEndRender = `${start.format('H:mm dddd Do')} - ${end.format('H:mm dddd Do MMMM YYYY')}`;
+    } else if (start.isSame(end, 'year')) {
+      startEndRender = `${start.format('H:mm dddd Do MMMM')} - ${end.format(
+        'H:mm dddd Do MMMM YYYY',
+      )}`;
+    } else {
+      startEndRender = `${start.format('H:mm dddd Do MMMM YYYY')} - ${end.format(
+        'H:mm dddd Do MMMM YYYY',
+      )}`;
+    }
   }
   return startEndRender;
 };
