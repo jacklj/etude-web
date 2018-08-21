6bbd412fe2120afbe00f767028bbf643fe3945f9
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-08-21 13:38:26 +0100

    Display practice timer

--- src/components/PracticeSession/PracticeSession.jsx
+++ src/components/PracticeSession/PracticeSession.jsx
@@ -2,21 +2,49 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
 
 import { eventFetchRequest } from '../../redux/events/events.actions';
 import { selectEvent } from '../../redux/events/events.selectors';
-// import { renderDuration } from '../../services/datetime';
+
+momentDurationFormatSetup(moment);
 
 class PracticeSession extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elapsed: undefined,
+    };
+
+    this.tick = this.tick.bind(this);
+  }
+
   componentDidMount() {
     // getPracticeSession to ensure it's up to date in the store (e.g. if user navigates
     // directly to a specific lesson page, so all lessons aren't already in the
     // store)
-    console.log(this.props.eventId)
     this.props.eventFetchRequest(this.props.eventId);
+    this.timer = setInterval(this.tick, 50);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    const { practiceSession } = this.props;
+    if (practiceSession) {
+      const start = moment(this.props.practiceSession.start);
+      const now = moment();
+      const diff = now.diff(start);
+      const elapsed = moment.duration(diff);
+      this.setState({ elapsed });
+    }
   }
 
   render() {
+    const { elapsed } = this.state;
     let jsx;
     if (!this.props.practiceSession) {
       jsx = <div>Loading</div>;
@@ -26,6 +54,7 @@ class PracticeSession extends Component {
         <div>
           <h3>Practice session</h3>
           {start}
+          <div>{elapsed ? elapsed.format('h:m:ss') : null}</div>
         </div>
       );
     }
