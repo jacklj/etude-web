db1358c6e7463f4ce80633f9f1668a52762f9ab0
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-09-29 12:52:46 +0100

    Clean up

--- src/redux/reduxOrm/selectors/events.selectors.js
+++ src/redux/reduxOrm/selectors/events.selectors.js
@@ -5,16 +5,10 @@ import orm from '../orm';
 const dbStateSelector = state => state.db;
 
 export const selectAllEvents = createSelector(
-  orm,
-  // The first input selector should always select the db-state.
-  // Behind the scenes, `createSelector` begins a Redux-ORM session
-  // with the value returned by `dbStateSelector` and passes
-  // that Session instance as an argument instead.
+  orm, // first input for redux-orm selector must be the db state
   dbStateSelector,
   session => session.Events.all().toModelArray().map(event => {
-    // Returns a reference to the raw object in the store,
-    // so it doesn't include any reverse or m2m fields.
-    const obj = event.ref;
+    const obj = event.ref; // reference to raw object in the store
 
     // resolve foreign keys
     const teacher = event.teacher_id && event.teacher_id.ref;
