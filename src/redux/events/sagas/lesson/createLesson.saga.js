fc22404ab43b2f1124374adf9922b6636d164336
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-09-29 20:13:15 +0100

    Create lesson generator handles HTTP response codes correctly and correctly gets the new lesson from the normalized response

--- src/redux/events/sagas/lesson/createLesson.saga.js
+++ src/redux/events/sagas/lesson/createLesson.saga.js
@@ -3,20 +3,28 @@ import { push } from 'connected-react-router';
 
 import { createLesson } from '../../../../services/api';
 import { lessonCreateSuccess, lessonCreateFailure, ACTION_TYPES } from '../../events.actions';
+import { EVENT_TYPES } from '../../../../constants';
 
 function* createLessonGenerator() {
+  const newEvent = {
+    type: EVENT_TYPES.LESSON,
+  };
   try {
-    const response = yield call(createLesson);
-    const actionToDispatch = lessonCreateSuccess(response);
-    yield put(actionToDispatch);
+    const response = yield call(createLesson, newEvent);
+    const body = yield response.json();
+    if (response.status === 200) {
+      yield put(lessonCreateSuccess(body));
 
-    // navigate to lesson page
-    // TODO 29th September 2018. Normalised response requires Object.keys(...)[0] - is this good?
-    const { event_id: eventId } = Object.keys(response.events)[0];
-    yield put(push(`/lesson/${eventId}`));
+      // TODO 29th September 2018. Normalised response requires
+      // Object.values(...)[0] - is this good?
+      const { event_id: eventId } = Object.values(response.events)[0];
+      // navigate to lesson page
+      yield put(push(`/lesson/${eventId}`));
+    } else {
+      yield put(lessonCreateFailure(body));
+    }
   } catch (e) {
-    const actionToDispatch = lessonCreateFailure(e);
-    yield put(actionToDispatch);
+    yield put(lessonCreateFailure(e));
   }
 }
 
