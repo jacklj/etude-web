bda22eb3faf3a9956803068e08756eec4e2ce4d6
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-10-04 12:28:55 +0100

    Move flags reducer to flags dir

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -5,7 +5,7 @@ import people from './people/people.reducer';
 import notes from './notes/notes.reducer';
 import db from './reduxOrm/db.reducer';
 import repOrExerciseInstances from './repOrExerciseInstances/repOrExerciseInstances.reducer';
-import flags from './reducers/flags.reducer';
+import flags from './flags/flags.reducer';
 
 export default combineReducers({
   events,
