77530499814641a554d2f93a15e80cda47a51c18
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-10-04 12:31:41 +0100

    Rename model files to x.model.js

--- src/redux/reduxOrm/orm.js
+++ src/redux/reduxOrm/orm.js
@@ -1,13 +1,13 @@
 import { ORM } from 'redux-orm';
-import Events from './models/Events';
-import Locations from './models/Locations';
-import Notes from './models/Notes';
-import People from './models/People';
-import Repertoire from './models/Repertoire';
-import Exercises from './models/Exercises';
-import RepOrExerciseInstances from './models/RepOrExerciseInstances';
-import OtherRepToWorkOn from './models/OtherRepToWorkOn';
-import PeopleAtEvents from './models/PeopleAtEvents';
+import Events from './models/Events.model';
+import Locations from './models/Locations.model';
+import Notes from './models/Notes.model';
+import People from './models/People.model';
+import Repertoire from './models/Repertoire.model';
+import Exercises from './models/Exercises.model';
+import RepOrExerciseInstances from './models/RepOrExerciseInstances.model';
+import OtherRepToWorkOn from './models/OtherRepToWorkOn.model';
+import PeopleAtEvents from './models/PeopleAtEvents.model';
 
 const orm = new ORM();
 orm.register(
