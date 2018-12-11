24bae33ea52336edbb2d017f4d168c2273599cb8
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-12-11 14:57:17 +0000

    Add units to graph Y axis and tooltip

--- src/components/Dashboard/SimpleLineChart.jsx
+++ src/components/Dashboard/SimpleLineChart.jsx
@@ -7,11 +7,14 @@ import LineChart from 'recharts/lib/chart/LineChart';
 import Line from 'recharts/lib/cartesian/Line';
 import XAxis from 'recharts/lib/cartesian/XAxis';
 import YAxis from 'recharts/lib/cartesian/YAxis';
+import Label from 'recharts/lib/component/Label';
 import CartesianGrid from 'recharts/lib/cartesian/CartesianGrid';
 import Tooltip from 'recharts/lib/component/Tooltip';
 import Legend from 'recharts/lib/component/Legend';
 
 import { calculateGraphData } from '../../redux/events/events.selectors';
+
+const renderTooltipValue = value => `${value} mins`;
 
 function SimpleLineChart({ graphData }) {
   return (
@@ -19,9 +22,11 @@ function SimpleLineChart({ graphData }) {
     <ResponsiveContainer width="99%" height={320}>
       <LineChart data={graphData}>
         <XAxis dataKey="name" />
-        <YAxis />
+        <YAxis>
+          <Label value="Minutes" angle={-90} offset={-15} position="left" />
+        </YAxis>
         <CartesianGrid vertical={false} strokeDasharray="3 3" />
-        <Tooltip />
+        <Tooltip formatter={renderTooltipValue} />
         <Legend />
         <Line type="monotone" dataKey="Practice Time" stroke="#82ca9d" activeDot={{ r: 8 }} />
       </LineChart>
