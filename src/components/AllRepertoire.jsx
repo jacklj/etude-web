fd7b8ab393228f9423ab666d5305e019000b2471
Jack Lawrence-Jones <jackvlj@gmail.com>
2018-12-31 15:22:49 +0000

    Get all repertoire when you mount the allRep page

--- src/components/AllRepertoire.jsx
+++ src/components/AllRepertoire.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import MUIDataTable from 'mui-datatables';
 
 import { selectAllRepertoireForTable } from '../redux/repertoire/repertoire.selectors';
+import { getAllRepertoireRequest } from '../redux/repertoire/repertoire.actions';
 
 const columns = [
   'ID',
@@ -21,23 +22,37 @@ const options = {
   filterType: 'checkbox',
 };
 
-const AllRepertoire = ({ allRepertoire }) => (
-  <div>
-    <MUIDataTable
-      title="Repertoire"
-      data={allRepertoire}
-      columns={columns}
-      options={options}
-    />
-  </div>
-);
+class AllRepertoire extends Component {
+  componentDidMount() {
+    this.props.getAllRepertoireRequest();
+  }
+
+  render() {
+    const { allRepertoire } = this.props;
+    return (
+      <div>
+        <MUIDataTable
+          title="Repertoire"
+          data={allRepertoire}
+          columns={columns}
+          options={options}
+        />
+      </div>
+    );
+  }
+}
 
 AllRepertoire.propTypes = {
   allRepertoire: PropTypes.array.isRequired, // eslint-disable-line react/forbid-prop-types
+  getAllRepertoireRequest: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   allRepertoire: selectAllRepertoireForTable(state),
 });
 
-export default connect(mapStateToProps)(AllRepertoire);
+const mapDispatchToProps = {
+  getAllRepertoireRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllRepertoire);
